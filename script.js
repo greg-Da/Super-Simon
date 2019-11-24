421d9575b83046b4cdbc3f7227d66692a9aad079
greg-Da <gregoiredalmasso@gmail.com>
2019-11-24 15:31:52 +0100

    comments + design + display note

--- script.js
+++ script.js
@@ -1,48 +1,81 @@
+//get the 4 colors
 const red = document.getElementsByClassName("red")[0];
 const blue = document.getElementsByClassName("blue")[0];
 const green = document.getElementsByClassName("green")[0];
 const yellow = document.getElementsByClassName("yellow")[0];
+//get the btn start & reset & restart
 const start = document.getElementById("start");
-const restart = document.getElementById("restart");
 const reset = document.getElementById("reset");
+const restart = document.getElementById("restart");
+//get  <h1> which will display the current turn
 const turn = document.getElementById("turn");
+//get the note displayer
+const note = document.getElementById("note");
+// get <p> to display the score during the game and at the game over
 const idRound = document.getElementById("round");
 const idScore = document.getElementById("score");
-
-const modal = document.getElementById("modalInfo");
-
-const info = document.getElementById("info");
-
-
-const span = document.getElementsByClassName("close")[0];
-
-const gameOver = document.getElementById("modalGameOver");
-const modalReset = document.getElementById("modalReset");
 const modalScore = document.getElementById("modalScore");
 const modalRound = document.getElementById("modalRound");
 
+//get the info modal component
+const modal = document.getElementById("modalInfo");
+const info = document.getElementById("info");
+const span = document.getElementsByClassName("close")[0];
+
+//get the game over modal component
+const gameOver = document.getElementById("modalGameOver");
+const modalReset = document.getElementById("modalReset");
+
+//Number of colors in a sequence
 let nmbColor = 4;
+
+//Number of round
 let round = 1;
+
+//The level of speed of the game
 let speed = 0;
+
+//The sequence made by the computer
 let simon = [];
+
+//The sequence made by the player
 let listPlayer = [];
+
+//If the Btn reset is pressed, the game will stop
 let stop = false;
+
+//The total number of the player's moves which are validated
 let goodMoves = 0;
+
+//The nimber of moves the player did during the round
 let playerMove = 0;
+
+//Check if that's the player's turn it is
 let pTurn = false;
 
+//The information displayed at the begining of the game
 turn.innerHTML = "Press Start";
-
-idScore.innerHTML = "0";
-idRound.innerHTML = round;
+idScore.innerHTML ="Score : " + goodMoves;
+idRound.innerHTML ="Round : " + round;
 
 
 async function IATurn() {
+	//Stop the player turn
 	pTurn = false;
+
+	//Display IA turn
 	turn.innerHTML = "IA Turn";
+
+	//Create a loop until the sequence is long enough
 	for (let i = 0; i != nmbColor; i++) {
+
+		//Stop the function if reset is pressed
 		if (stop) {return}
-			simon[i] = Math.floor(Math.random() * 4);;
+
+			//Choose a random number and insert is value in simon[]
+			simon[i] = Math.floor(Math.random() * 4);
+
+		//Call the animation of the random value
 		switch (simon[i]) {
 			case 0:
 			redLight();
@@ -64,8 +97,10 @@ async function IATurn() {
 			console.log(simon);
 			break;
 		};
+		//Wait 1 sec between hightlighting colors for the first turn and increase the speed after
 		await Wait(1000 - (round * 50));
 	}
+	//Allow the player turn and display it
 	pTurn = true;
 	turn.innerHTML = "Your Turn";
 }
@@ -73,114 +108,164 @@ async function IATurn() {
 
 
 function playerTurn() {
+	//Stop the function if reset is pressed
 	if (stop) {return}
-	red.addEventListener("click", () => {
-		if (pTurn) {
-		let audio = new Audio('sound/do.wav');
-		audio.play();
-		listPlayer[playerMove] = 0;
-		if (listPlayer[playerMove] != simon[playerMove]) {
-			hasLost();
-		}
-		else {
-			playerMove++;
-			goodMoves++;
-			idScore.innerHTML = goodMoves;
-		}
-		redLight();
-		if(listPlayer.length === nmbColor){
-			Compare();
-		}
-	}
-	})
 
-	blue.addEventListener("click", () => {
+	//If red is pressed
+	red.addEventListener("click", () => {
+		
+		//Check is this is the player's turn
 		if (pTurn) {
-		let audio = new Audio('sound/re.wav');
-		audio.play();
-		listPlayer[playerMove] = 1;
-		if (listPlayer[playerMove] != simon[playerMove]) {
-			hasLost();
+
+			//Enter the value in listPlayer
+			listPlayer[playerMove] = 0;
+
+			//Check if the color picked is correct and increase the score and display it
+			if (listPlayer[playerMove] === simon[playerMove]) {
+				playerMove++;
+				goodMoves++;
+				idScore.innerHTML ="Score : " + goodMoves;
+			}
+
+			//If not call the function hasLost
+			else {
+				hasLost();
+			}
+
+			//Call the animation
+			redLight();
+
+			//When the length of playerList[] is the same size as the sequence we will compare them 
+			if(listPlayer.length === nmbColor){
+				Compare();
+			}
 		}
-		else {
-			playerMove++;
-			goodMoves++;
-			idScore.innerHTML = goodMoves;
+	})
+		
+	blue.addEventListener("click", () => {
+		
+		//Check is this is the player's turn
+		if (pTurn) {
+
+			//Enter the value in listPlayer
+			listPlayer[playerMove] = 1;
+
+			//Check if the color picked is correct and increase the score and display it
+			if (listPlayer[playerMove] === simon[playerMove]) {
+				playerMove++;
+				goodMoves++;
+				idScore.innerHTML ="Score : " + goodMoves;
+			}
+			//If not call the function hasLost
+			else {
+				hasLost();
+			}
+
+			//Call the animation
+			blueLight();
+
+			//When the length of playerList[] is the same size as the sequence we will compare them 
+			if(listPlayer.length === nmbColor){
+				Compare();
+			}
 		}
-		blueLight();
-		if(listPlayer.length === nmbColor){
-			Compare();
-		}
-	}
 	})
 
 	green.addEventListener("click", () => {
+		
+		//Check is this is the player's turn
 		if (pTurn) {
-		let audio = new Audio('sound/mi.wav');
-		audio.play();
-		listPlayer[playerMove] = 2;
-		if (listPlayer[playerMove] != simon[playerMove]) {
-			hasLost();
+
+			//Enter the value in listPlayer
+			listPlayer[playerMove] = 2;
+
+			//Check if the color picked is correct and increase the score and display it
+			if (listPlayer[playerMove] === simon[playerMove]) {
+				playerMove++;
+				goodMoves++;
+				idScore.innerHTML ="Score : " + goodMoves;
+			}
+			//If not call the function hasLost
+			else {
+				hasLost();
+			}
+
+			//Call the animation
+			greenLight();
+
+			//When the length of playerList[] is the same size as the sequence we will compare them 
+			if(listPlayer.length === nmbColor){
+				Compare();
+			}
 		}
-		else {
-			playerMove++;
-			goodMoves++;
-			idScore.innerHTML = goodMoves;
-		}
-		greenLight();
-		if(listPlayer.length === nmbColor){
-			Compare();
-		}
-	}
 
 	})
 
 	yellow.addEventListener("click", () => {
+		
+		//Check is this is the player's turn
 		if (pTurn) {
-		let audio = new Audio('sound/fa.wav');
-		audio.play();
-		listPlayer[playerMove] = 3;
-		if (listPlayer[playerMove] != simon[playerMove]) {
-			hasLost();
+
+			//Enter the value in listPlayer
+			listPlayer[playerMove] = 3;
+
+			//Check if the color picked is correct and increase the score and display it
+			if (listPlayer[playerMove] === simon[playerMove]) {
+				playerMove++;
+				goodMoves++;
+				idScore.innerHTML ="Score : " + goodMoves;
+
+			}
+			//If not call the function hasLost
+			else {
+				hasLost();
+			}
+
+			//Call the animation
+			yellowLight();
+
+			//When the length of playerList[] is the same size as the sequence we will compare them 
+			if(listPlayer.length === nmbColor){
+				Compare();
+			}
 		}
-		else {
-			playerMove++;
-			goodMoves++;
-			idScore.innerHTML = goodMoves;
-		}
-		yellowLight();
-		if(listPlayer.length === nmbColor){
-			Compare();
-		}
-	}
 
 	})
 }
 
 
+//Turn both sequence into string and compare them if their the same call the function hasWin else hasLost
 function Compare() {
 	simonArray = simon.join();
 	playerArray = listPlayer.join();
 	if (simonArray === playerArray) {
-		console.log("WIN")
 		hasWon();
+	}
+	else{
+		hasLost();
 	}
 }
 
+//The player won the count of rounds, the speed & the size of the sequence increases
 async function hasWon() {
 	round++;
 	speed++;
+	//The speed can't go faster than level 8
 	if (speed > 8) {
 		speed = 8;
 	}
 	nmbColor++;
+	//Both arrays and playerMoves are set zero/empty
 	playerMove = 0;
 	listPlayer = [];
-	idRound.innerHTML = round;
+	//Display the new round
+	idRound.innerHTML ="Round : " + round;
+	//Wait 1 sec and launch a new IATurn
 	await Wait(1000);
 	await IATurn();
 }
 
+//The player lost so the game over modal show up with the score and a button reset
 function hasLost() {
 	if (modalGameOver.style.display === "none") {
 		modalGameOver.style.display = "block";
@@ -188,6 +273,7 @@ function hasLost() {
 		modalScore.innerHTML = "Score : " + goodMoves;
 	}
 
+	//Click on reset btn to hide the modal and reset the game
 	modalReset.addEventListener("click", () => {
 		modalGameOver.style.display = "none";
 		Reset();
@@ -196,56 +282,86 @@ function hasLost() {
 
 
 
+//Create the animation and sound of the color
 function redLight() {
+	note.innerHTML = "DO";
+	note.style.visibility = "visible";
 	let DO = new Audio('sound/do.wav');
 	DO.play();
 	red.classList.add("red--Active");
 	setTimeout(function(){
 		red.classList.remove("red--Active");
 	},500);
+	setTimeout(function(){
+		note.style.visibility = "hidden"
+	},500);
 }
 
+//Create the animation and sound of the color
 function blueLight() {
+	note.innerHTML = "RE";
+	note.style.visibility = "visible";
 	let RE = new Audio('sound/re.wav');
 	RE.play();
 	blue.classList.add("blue--Active");
 	setTimeout(function(){
 		blue.classList.remove("blue--Active");
 	},500);
+	setTimeout(function(){
+		note.style.visibility = "hidden"
+	},500);
 }
 
+//Create the animation and sound of the color
 function greenLight() {
+	note.innerHTML = "MI";
+	note.style.visibility = "visible";
 	let MI = new Audio('sound/mi.wav');
 	MI.play();
 	green.classList.add("green--Active");
 	setTimeout(function(){
 		green.classList.remove("green--Active");
 	},500);
+	setTimeout(function(){
+		note.style.visibility = "hidden"
+	},500);
 }
 
+//Create the animation and sound of the color
 function yellowLight() {
+	note.innerHTML = "FA";
+	note.style.visibility = "visible";
 	let FA = new Audio('sound/fa.wav');
 	FA.play();
 	yellow.classList.add("yellow--Active");
 	setTimeout(function(){
 		yellow.classList.remove("yellow--Active");
 	},500);
+	setTimeout(function(){
+		note.style.visibility = "hidden"
+	},500);
 }
 
+//Function to create an async setTimeout
 async function Wait(ms){
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+//When the game start for the first time it destroys the start bouton, and launch the IA Turn and after the player's Turn
 async function Start() {
 	start.style.display = 'none';
 	await IATurn();
 	await playerTurn();
 }
 
+/*If the game has been reset and a new game is started this function will be called, it destroys the start bouton, 
+and launch the IA Turn and not the function playerTurn which is already running in background*/
 async function Restart() {
 	restart.style.display = 'none';
 	await IATurn();
 }
 
+//Stop the current game and totaly reset the game
 function Reset() {
 	stop = true;
 	turn.innerHTML = "Press Start";
@@ -257,11 +373,14 @@ function Reset() {
 	simon = [];
 	listPlayer = [];
 	pTurn = false;
-	restart.style.display = 'inline';
-	idScore.innerHTML = "0";
-	idRound.innerHTML = round;
+	if (start.style.display = 'none';) {
+		restart.style.display = 'inline';
+	}
+	idScore.innerHTML = "Score : " + goodMoves;
+	idRound.innerHTML ="Round : " + round;
 }
 
+//When clicked pass the stop varaible to false & call a function to start the game
 start.onclick = function() {
 
 	stop = false;
@@ -269,6 +388,7 @@ start.onclick = function() {
 	
 };
 
+//When clicked pass the stop varaible to false & call a function to restart the game
 restart.onclick = function() {
 
 	stop = false;
@@ -276,9 +396,10 @@ restart.onclick = function() {
 	
 };
 
+//When clicked call the Reset function
 reset.onclick = function() {
 
-	Reset();
+	hasLost();
 	
 };
 
